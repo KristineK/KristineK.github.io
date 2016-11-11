c9c1299ea6a30855e6a189f7834598d1a949b01d
Kristine <kristine.kronberga@accenture.com>
2016-11-11 04:49:05 +0200

    some drafts 3

--- scripts1.js
+++ scripts1.js
@@ -11,3 +11,63 @@ window.onclick = function(event) {
     show("sync_menu", false);
   }
 } 
+
+function numberValidation() {
+    show("ch1_error", false);
+    var x, text, giveAnswer;
+
+    x = document.getElementById("numb").value;
+    showError = true;
+
+    if (!x || 0 === x.length || /^\s*$/.test(x)) {
+        text = "You haven't entered anything";
+    } else if (isNaN(x)) {
+        text = "Please enter a number";
+    } else if (x < 49) {
+        text = "Number is too small";
+    } else if (x > 101) {
+        text = "Number is too big";
+    } else {
+        showError = false;
+        alert("Square of " + x + " is " + Math.sqrt(x).toFixed(2));
+    }
+    if (showError) {
+      show("ch1_error", true);
+      document.getElementById("ch1_error").innerHTML = text;
+    }
+}
+
+
+function alertSummoning() {
+    show("textForAlerts", false);
+    alert("I am an alert box!");
+}
+
+function conformationOrDenial() {
+    show("textForAlerts", true);
+    var x;
+    if (confirm("Press a button!") == true) {
+        x = "Why on earth have you agreed to it?!";
+    } else {
+        x = "You have dared to deny me!";
+    }
+    document.getElementById("textForAlerts").innerHTML = x;
+}
+
+function clickToEnterNumber() {
+    show("textForAlerts", true);
+    var text;
+    var number = prompt("Please enter a number", "");
+    var randomNumber = Math.random();
+    
+    if (number != null) {
+        if (number == randomNumber) {
+          text = "Yes, the number is indeed " + number + "! How are you today?";
+        } else if (number == 42) {
+          text = "Now that you know the answer, do you remeber the question?"
+        } else {
+          text = "Wrong! It is " + randomNumber + " instead of " + number
+        }
+        document.getElementById("textForAlerts").innerHTML = text;
+    }
+}
