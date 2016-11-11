704be931c9b64f0dccba61a18cc1a416db5fb6c2
Kristine <kristine.kronberga@accenture.com>
2016-11-11 05:15:34 +0200

    some drafts 4

--- scripts1.js
+++ scripts1.js
@@ -6,7 +6,6 @@ function show(id, value) {
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.menu_button')) {
-    var dropdowns = document.getElementsByClassName("sub-nav");
     show("base_menu", false);
     show("sync_menu", false);
   }
