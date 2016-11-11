77062e81272c4f565d5702b7fc8ca8d1c09d954a
Kristine <kristine.kronberga@accenture.com>
2016-11-11 03:05:02 +0200

    some drafts 2

--- scripts1.js
+++ scripts1.js
@@ -1,25 +1,13 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    show("base_menu", "show")
-    // document.getElementById("base_menu").classList.toggle("show");
-}
 
 function show(id, value) {
-    document.getElementById(id).style.visibility = value ? 'visible' : 'hidden';
+  document.getElementById(id).style.visibility = value ? 'visible' : 'hidden';
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+  if (!event.target.matches('.menu_button')) {
+    var dropdowns = document.getElementsByClassName("sub-nav");
+    show("base_menu", false);
+    show("sync_menu", false);
   }
 } 
